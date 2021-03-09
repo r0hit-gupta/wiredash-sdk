14cdaa394a2e67e963ba3300441ce3eb0fe378c9
Rohit <rohit1997gupta@gmail.com>
2021-03-09 16:10:56 +0530

    Fix erros

--- lib/src/common/network/api_client.dart
+++ lib/src/common/network/api_client.dart
@@ -11,18 +11,18 @@ class ApiClient {
   });
 
   // static const String _host = 'http://localhost:5001/wiredash-75f17/us-central1/app';
-  static const String _host = 'https://us-central1-wiredash-75f17.cloudfunctions.net/app/';
+  static const String _host =
+      'https://us-central1-wiredash-75f17.cloudfunctions.net/app/';
 
   final Client httpClient;
   final String projectId;
   final String secret;
 
   Future<Map<String, dynamic>> get(String urlPath) async {
-    final url = '$_host$urlPath';
-    final BaseResponse response = await httpClient.get(url, headers: {
-      'project': projectId,
-      'authorization': secret
-    });
+    final url = Uri.parse('$_host$urlPath');
+
+    final BaseResponse response = await httpClient
+        .get(url, headers: {'project': projectId, 'authorization': secret});
     final responseString = utf8.decode((response as Response).bodyBytes);
     if (response.statusCode != 200) {
       throw Exception('${response.statusCode}:\n$responseString');
@@ -39,14 +39,14 @@ class ApiClient {
     @required Map<String, String> arguments,
     List<MultipartFile> files,
   }) async {
-    final url = '$_host$urlPath';
+    final url = Uri.parse('$_host$urlPath');
     BaseResponse response;
     String responseString;
     arguments.removeWhere((key, value) => value == null || value.isEmpty);
     files.removeWhere((element) => element == null);
 
     if (files != null && files.isNotEmpty) {
-      final multipartRequest = MultipartRequest('POST', Uri.parse(url))
+      final multipartRequest = MultipartRequest('POST', url)
         ..fields.addAll(arguments)
         ..files.addAll(files);
       multipartRequest.headers['project'] = projectId;
@@ -58,10 +58,7 @@ class ApiClient {
     } else {
       response = await httpClient.post(
         url,
-        headers: {
-          'project': projectId,
-          'authorization': secret
-        },
+        headers: {'project': projectId, 'authorization': secret},
         body: arguments,
       );
       responseString = utf8.decode((response as Response).bodyBytes);
